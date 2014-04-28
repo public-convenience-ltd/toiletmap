6f0b03db63156d29fdc0d076aa8c1fecdb3a5d5e
Rupert Redington <rupert@neontribe.co.uk>
2014-04-28 12:18:13 +0100

    Bounce favicon requests

--- config/koa.js
+++ config/koa.js
@@ -4,6 +4,7 @@ var fs = require('fs'),
     path = require('path'),
     logger = require('koa-logger'),
     router = require('koa-router'),
+    favicon = require('koa-favicon'),
     serializer = require('../api/koa-serializer'),
     config = require('./config');
 
@@ -12,8 +13,9 @@ module.exports = function(app){
         app.use(logger());
     }
 
+    app.use(favicon()); // Bounce annoying favicon requests with a 404
     app.use(serializer());
-    app.use(router(app));
+    app.use(router(app)); 
 
     // mount all the routes defined in the api/controllers
     fs.readdirSync(path.join(config.app.root, 'api', 'controllers')).forEach(function(file){
