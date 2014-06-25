31ebc5a8ad4740f253a5dd1500994932998cc21c
Rupert Redington <rupert@neontribe.co.uk>
2014-06-25 17:16:39 +0100

    GBPTM MAster CSV

--- data/importers/gbptm_csv.sh
+++ data/importers/gbptm_csv.sh
@@ -7,15 +7,7 @@ while getopts "vV" options; do
     esac
 done
 
-../../cli/fetch -f ../fetchers/gbptm_csv ../sources/gbptm_1.csv \
- | \
- ../../cli/transform -t ../transformers/convert_properties.js gbptm \
-| \
-../../cli/transform -t ../transformers/attribute.js attribution 'Great British Public Toilet Map' \
- | \
- ../../cli/write $VERBOSE $VVERBOSE -w ../writers/process_loo_report
-
- ../../cli/fetch -f ../fetchers/gbptm_csv ../sources/gbptm_2.csv \
+../../cli/fetch -f ../fetchers/gbptm_csv ../sources/gbptm.csv \
  | \
  ../../cli/transform -t ../transformers/convert_properties.js gbptm \
 | \
