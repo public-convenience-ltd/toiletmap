986ec770e659b2681cce5a72ffda80aa5879db19
Rupert Redington <rupert@neontribe.co.uk>
2016-06-18 09:41:29 +0100

    Begin updating dependencies

--- config/mongo.js
+++ config/mongo.js
@@ -2,7 +2,9 @@
 
 var mongoose = require('mongoose')
 var config = require('./config')
+var bluebird = require('bluebird')
 
+mongoose.Promise = bluebird
 mongoose.connect(config.mongo.url)
 
 module.exports = mongoose.connection
