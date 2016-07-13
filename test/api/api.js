03f5a6f269a9fb5191f867db29a6e9878448b0ae
Ruby  Ferguson <adamdanferguson@gmail.com>
2016-07-13 09:58:05 +0100

    did test for get/:id

--- test/api/api.js
+++ test/api/api.js
@@ -12,14 +12,16 @@ var Loo = require('../../models/loo')
 
 Loo.remove = thunk(Loo.remove)
 
-describe('Loos service', function () {
+describe('Find in polygon', function () {
   // Bring up a server before testing
   before(function (done) {
     co(function * () {
       yield app.init()
       // Add 12 fake loos
       yield _.map(_.range(12), function () {
-        return fakery.makeAndSave('loo')
+	
+        result =  fakery.makeAndSave('looBox');
+	return result
       })
     }).then(done)
   })
@@ -46,16 +48,83 @@ describe('Loos service', function () {
     .end(done)
   });
 
-  it('loos/near/:lon/:lat should return an array of loos', function (done) {
+})
+
+describe('Find in circle', function () {
+  // Bring up a server before testing
+  before(function (done) {
+    co(function * () {
+      yield app.init()
+      // Add 5 fake loos
+      yield _.map(_.range(5), function () {
+        result =  fakery.makeAndSave('looCircle');
+	return result
+      })
+    }).then(done)
+  })
+  // tear it down after
+  after(function (done) {
+    app.server.tclose = thunk(app.server.close)
+    co(function * () {
+      yield app.server.tclose()
+      yield Loo.remove({})
+    }).then(done)
+  })
+  
+
+  it('/loos/near/:lon/:lat', function (done) {
     request
-    .get('/loos/near/-22.2/52.5')
+    .get('/loos/near/0/0')
     .set('Accept', 'application/json')
     .expect(200)
     .expect(function (res) {
-      if (!(res.body.features.length === 0)) {
+      if (!(res.body.features.length === 5)) {
         return 'Not enough Loos'
       }
     })
     .end(done)
   });
+
 })
+
+describe('Find Via ID', function () {
+  // Bring up a server before testing
+  var looGlobal = null;
+  before(function (done) {
+    co(function * () {
+      yield app.init()
+      //yield result = fakery.makeAndSave('looWithID');
+      yield fakery.makeAndSave('looWithID', function(err, loo) {
+	looGlobal = loo;
+	result = loo;
+	done()
+	return result
+      });
+    })
+  })
+  // tear it down after
+  after(function (done) {
+    app.server.tclose = thunk(app.server.close)
+    co(function * () {
+      yield app.server.tclose()
+      yield Loo.remove({})
+    }).then(done)
+  })
+  
+
+  it('/loos/:id', function (done) {
+    stringID = looGlobal._id.toString();
+    request
+    .get('/loos/'+stringID)
+    .set('Accept', 'application/json')
+    .expect(200)
+    .expect(function (res) {
+	console.log(res.body);
+    })
+    .end(done)
+  });
+
+})
+
+
+
--- test/fixtures.js
+++ test/fixtures.js
@@ -1,22 +1,61 @@
 var fakery = require('mongoose-fakery')
 var thunk = require('thunkify')
 var Loo = require('../models/loo')
+var mongoose = require('mongoose');
 
-fakery.generator('lonlat', function (bounds) {
+
+//generators
+
+//Generates random loos with box
+fakery.generator('lonlatBox', function (bounds) {
   var bbox = bounds || [60.85, 2.69, 49.84, -9.23]
   function randFloat (min, max, precision) {
     return parseFloat(Math.min(min + (Math.random() * (max - min)), max).toFixed(precision))
   }
-  return [randFloat(bbox[3], bbox[1], 5), randFloat(bbox[2], bbox[0], 5)]
+  result =  [randFloat(bbox[3], bbox[1], 5), randFloat(bbox[2], bbox[0], 5)]
+  return result
 })
 
-fakery.fake('loo', Loo, {
+//generates random loos within a cirle -- technically not correct, looking for better alternative
+fakery.generator('lonlatCircle', function (bounds) {
+  var boundaries = bounds || [0.007,0,0] //[radius,long,lat]
+  function randFloat (min, max, precision) {
+    return parseFloat(Math.min(min + (Math.random() * (max - min)), max).toFixed(precision))
+  }
+
+ result = [randFloat(0,boundaries[0],5)*Math.sin(randFloat(0,6.28,5))+ boundaries[1],randFloat(0,boundaries[0],5)*Math.sin(randFloat(0,6.28,5))+boundaries[2]];
+  return result
+})
+
+
+
+
+//models to be faked
+fakery.fake('looBox', Loo, {
   geometry: {
     type: 'Point',
-    coordinates: fakery.g.lonlat()
+    coordinates: fakery.g.lonlatBox()
   }
 })
 
+fakery.fake('looCircle', Loo, {
+  geometry: {
+    type: 'Point',
+    coordinates: fakery.g.lonlatCircle()
+  }
+})
+
+//var newId = new mongoose.mongo.ObjectID('56cb91bdc3464f14678934ca')
+fakery.fake('looWithID', Loo, {
+  geometry: {
+    type: 'Point',
+    coordinates: fakery.g.lonlatCircle()
+  }
+})
+
+
+
 fakery.makeAndSave = thunk(fakery.makeAndSave)
+
 
 module.exports = fakery
