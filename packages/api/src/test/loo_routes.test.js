ed271422b4afebcf07a93f56fac80bb25096ab2a
Harry D <me@hoolean.com>
2018-06-25 16:25:09 +0100

    We can get away with just calling ensureIndexes() once before ALL of our tests here

--- packages/api/src/test/loo_routes.test.js
+++ packages/api/src/test/loo_routes.test.js
@@ -4,11 +4,14 @@ const Loo = require('../models/loo');
 const mongoose = require('mongoose');
 const looRadius = require('./data/looRadius.js');
 
+beforeAll(async () => {
+  await Loo.ensureIndexes();
+});
+
 describe('api loo routes (public)', () => {
   describe('/api/loos/near/:lon/:lat', () => {
     beforeAll(async () => {
       await Loo.collection.insertMany(looRadius);
-      await Loo.ensureIndexes();
     });
 
     it('should return a limit of 5 loos for -0.2068223/51.518342', async () => {
@@ -44,7 +47,6 @@ describe('api loo routes (public)', () => {
     const looBox = require('./data/looBox.js');
     beforeAll(async () => {
       await Loo.collection.insertMany(looBox);
-      await Loo.ensureIndexes();
     });
 
     it('should return 12 loos', async () => {
