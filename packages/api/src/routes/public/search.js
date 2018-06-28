8f145ccede9c2687ffe1beb3b44d45b89cb575c3
Harry D <me@hoolean.com>
2018-06-28 13:43:22 +0100

    Move to indexes for textual searches, limiting some of the search APIs flexibility as a compromise to stop ReDoS attacks

--- packages/api/src/routes/public/search.js
+++ packages/api/src/routes/public/search.js
@@ -13,26 +13,12 @@ router.get('/', async (req, res) => {
   delete params.page;
 
   if (params.text) {
-    query.$or = [
-      { 'properties.name': new RegExp('.*' + req.query.text + '.*', 'i') },
-      { 'properties.notes': new RegExp('.*' + req.query.text + '.*', 'i') },
-    ];
+    query.$or = [{ $text: { $search: req.query.text } }];
   }
   delete params.text;
 
-  // Handle text searches
-  _.each(params, function(val, name) {
-    query.$and = query.$and || [];
-    if (/^text_/.test(name)) {
-      query.$and.push({
-        ['properties.' + name.replace('text_', '')]: new RegExp(
-          '.*' + val + '.*',
-          'i'
-        ),
-      });
-      delete params[name];
-    }
-  });
+  // Arbitrary text searches have been removed until a way is found that is not
+  // prone to ReDoS attacks or indexing every possible property by text
 
   // Handle queries for missing fields
   _.each(params, function(val, name) {
