5944660f220c1c66c8537511a222615c858544c3
Rupert Redington <rupert@neontribe.co.uk>
2018-06-28 12:50:01 +0100

    fix: silly reversed ternary

---
+++ packages/ui/src/Auth.js
@@ -0,0 +1,75 @@
+import auth0 from 'auth0-js';
+import history from './history';
+
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: 'gbptm.eu.auth0.com',
+    clientID: 'sUts4RKy04JcyZ2IVFgMAC0rhPARCQYg',
+    redirectUri:
+      process.env.NODE_ENV === 'production'
+        ? 'https://gbptm-unity.herokuapp.com/callback'
+        : 'http://localhost:3000/callback',
+    responseType: 'token id_token',
+    audience: 'https://gbptm-unity.herokuapp.com/api',
+    scope: 'openid profile report:loo',
+  });
+
+  constructor() {
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.getAccessToken = this.getAccessToken.bind(this);
+  }
+
+  handleAuthentication() {
+    return new Promise((resolve, reject) => {
+      this.auth0.parseHash((err, authResult) => {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          this.setSession(authResult);
+          resolve();
+        } else if (err) {
+          reject(err);
+        }
+      });
+    });
+  }
+
+  setSession(authResult) {
+    // Set the time that the Access Token will expire at
+    let expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  getAccessToken() {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('No access token found');
+    }
+    return accessToken;
+  }
+
+  login() {
+    this.auth0.authorize();
+  }
+
+  logout() {
+    // Clear Access Token and ID Token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replace('/');
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // Access Token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+}
