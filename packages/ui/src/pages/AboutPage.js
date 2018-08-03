8b398bd5c91d1fc80d03856ca3daa32e9ad1f17e
Ed Perkins <ed@edperkins.co.uk>
2018-08-03 10:29:40 +0100

    feat: add alternate coloured banding to about page text

--- packages/ui/src/pages/AboutPage.js
+++ packages/ui/src/pages/AboutPage.js
@@ -60,63 +60,71 @@ class AboutPage extends Component {
           </a>{' '}
           (who designed and built it).
         </p>
+        <div className={layout.bandedSection}>
+          <div className={layout.bandedSectionText}>
+            <div className={layout.mainContainer}>
+              <h2 id="use-our-loos" className={headings.regular}>
+                Use Our Loos
+              </h2>
 
-        <h2 id="use-our-loos" className={headings.regular}>
-          Use Our Loos
-        </h2>
+              <p>
+                We all need toilets. When you need to go, you need to go. At
+                home or in an office this isn’t a problem. However, out and
+                about, are you sure there’s somewhere to go when the need
+                arises?
+              </p>
 
-        <p>
-          We all need toilets. When you need to go, you need to go. At home or
-          in an office this isn’t a problem. However, out and about, are you
-          sure there’s somewhere to go when the need arises?
-        </p>
+              <p>
+                Across the UK, more than one in every three public toilets have
+                been closed over the last two decades. Some councils are already
+                without a single free-to-use public convenience.
+              </p>
 
-        <p>
-          Across the UK, more than one in every three public toilets have been
-          closed over the last two decades. Some councils are already without a
-          single free-to-use public convenience.
-        </p>
+              <p>
+                <strong>Use Our Loos</strong> is the first national community
+                toilet scheme, created by the British Toilet Association,
+                Domestos and the Great British Public Toilet Map, with the
+                ambition to unlock the loos that are hidden in high street
+                cafes, restaurants, coffee shops and bars and open them up to
+                the community.
+              </p>
 
-        <p>
-          <strong>Use Our Loos</strong> is the first national community toilet
-          scheme, created by the British Toilet Association, Domestos and the
-          Great British Public Toilet Map, with the ambition to unlock the loos
-          that are hidden in high street cafes, restaurants, coffee shops and
-          bars and open them up to the community.
-        </p>
+              <p>
+                As more public loos are closed, we’re asking that more loos are
+                made public.
+              </p>
 
-        <p>
-          As more public loos are closed, we’re asking that more loos are made
-          public.
-        </p>
+              <p>
+                We’re inviting businesses to sign up and welcome everyone,
+                customers and non-customers, to use their loos. In return, we’re
+                providing them with free Domestos product, directing people to
+                their venues and driving local fame so they will benefit from
+                increased customers and a stronger connection to their
+                community.
+              </p>
 
-        <p>
-          We’re inviting businesses to sign up and welcome everyone, customers
-          and non-customers, to use their loos. In return, we’re providing them
-          with free Domestos product, directing people to their venues and
-          driving local fame so they will benefit from increased customers and a
-          stronger connection to their community.
-        </p>
+              <p>
+                We’re inviting the public to use our map to discover loos near
+                them and to help us improve the service by adding new locations
+                or flagging loos that have closed.
+              </p>
 
-        <p>
-          We’re inviting the public to use our map to discover loos near them
-          and to help us improve the service by adding new locations or flagging
-          loos that have closed.
-        </p>
+              <h3 className={headings.small}>The war on poor sanitation</h3>
 
-        <h3 className={headings.small}>The war on poor sanitation</h3>
-
-        <p>
-          Domestos is proud to support the Great British Public Toilet Map.  Use
-          Our Loos marks the latest step in Domestos’ mission is to win the war
-          on poor sanitation. Worldwide, an estimated 2.5 billion people lack
-          access to proper sanitation while around 1 billion don’t have
-          sanitation facilities at all. Partnering with UNICEF, Domestos will
-          help 25 million people around the world enjoy improved access to
-          clean, safe, accessible toilets by 2020. People in the UK can further
-          support the mission by buying the Domestos bottle marked with the
-          UNICEF logo.
-        </p>
+              <p>
+                Domestos is proud to support the Great British Public Toilet
+                Map.  Use Our Loos marks the latest step in Domestos’ mission is
+                to win the war on poor sanitation. Worldwide, an estimated 2.5
+                billion people lack access to proper sanitation while around 1
+                billion don’t have sanitation facilities at all. Partnering with
+                UNICEF, Domestos will help 25 million people around the world
+                enjoy improved access to clean, safe, accessible toilets by
+                2020. People in the UK can further support the mission by buying
+                the Domestos bottle marked with the UNICEF logo.
+              </p>
+            </div>
+          </div>
+        </div>
 
         <h2 id="contributing" className={headings.regular}>
           Thinking about adding a toilet?
@@ -183,48 +191,53 @@ class AboutPage extends Component {
             <a href="mailto:gbtoiletmap@gmail.com">gbtoiletmap@gmail.com</a>.
           </strong>
         </p>
+        <div className={layout.bandedSection}>
+          <div className={layout.bandedSectionText}>
+            <div className={layout.mainContainer}>
+              <h2 id="privacy" className={headings.regular}>
+                Privacy Policy
+              </h2>
 
-        <h2 id="privacy" className={headings.regular}>
-          Privacy Policy
-        </h2>
+              <p>
+                Cookies on RCA web sites are used to gather anonymous data such
+                as which pages are viewed, what time the visit occurred, if the
+                visitor has been to the site before, and which site referred the
+                visitor to the web page etc.
+              </p>
 
-        <p>
-          Cookies on RCA web sites are used to gather anonymous data such as
-          which pages are viewed, what time the visit occurred, if the visitor
-          has been to the site before, and which site referred the visitor to
-          the web page etc.
-        </p>
+              <p>
+                The Royal College of Art also notes and saves information such
+                as time of day, browser type and content requested. That
+                information is used to provide more relevant services to users.
+              </p>
 
-        <p>
-          The Royal College of Art also notes and saves information such as time
-          of day, browser type and content requested. That information is used
-          to provide more relevant services to users.
-        </p>
+              <p>
+                Any data collected automatically from visitors to our web sites
+                through the use of cookies will be used to improve the sites,
+                and will not and cannot be used to identify any visitor. Consent
+                by site visitors for this anonymous data to be collected and
+                used by RCA for the purpose stated above will be assumed by your
+                continued navigation of this website.
+              </p>
 
-        <p>
-          Any data collected automatically from visitors to our web sites
-          through the use of cookies will be used to improve the sites, and will
-          not and cannot be used to identify any visitor. Consent by site
-          visitors for this anonymous data to be collected and used by RCA for
-          the purpose stated above will be assumed by your continued navigation
-          of this website.
-        </p>
+              <p>
+                We will not associate any data gathered from this site with any
+                personally identifying information from any source. We may also
+                log Internet Protocol (IP) address (but nothing that directly
+                identifies visitors) in order to receive and send the required
+                information over the internet.
+              </p>
 
-        <p>
-          We will not associate any data gathered from this site with any
-          personally identifying information from any source. We may also log
-          Internet Protocol (IP) address (but nothing that directly identifies
-          visitors) in order to receive and send the required information over
-          the internet.
-        </p>
-
-        <p>
-          For further details about the RCA’s policy on privacy and cookies,
-          please visit:<br />
-          <a href="http://www.rca.ac.uk/contact-us/about-this-website/privacy-cookies">
-            Privacy and Cookies
-          </a>
-        </p>
+              <p>
+                For further details about the RCA’s policy on privacy and
+                cookies, please visit:<br />
+                <a href="http://www.rca.ac.uk/contact-us/about-this-website/privacy-cookies">
+                  Privacy and Cookies
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
