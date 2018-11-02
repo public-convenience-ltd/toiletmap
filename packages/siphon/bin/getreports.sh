ab77832070da0b3c25915069a98e4bb19be485db
Rupert Redington <rupert@neontribe.co.uk>
2018-11-02 14:23:28 +0000

    build: remove siphon and sync versions

--- packages/siphon/bin/getreports.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [[ -z "$1" ]]
-then
-	echo "Usage ./getreports.sh <password>" >&2
-	exit 1
-fi
-
-mongoexport --host lamppost.9.mongolayer.com --port 10239 --type json --jsonArray --username heroku --db app29532998 --password "$1" --collection looreports --out reports.json
