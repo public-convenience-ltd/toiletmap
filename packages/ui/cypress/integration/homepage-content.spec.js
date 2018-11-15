b2acccb7aa6c1df38af40ef1e5504cc91d5a1f6b
edpe <ed@edperkins.co.uk>
2018-11-15 15:37:33 +0000

    test: click on a loo and go to page

--- packages/ui/cypress/integration/homepage-content.spec.js
+++ packages/ui/cypress/integration/homepage-content.spec.js
@@ -33,4 +33,9 @@ describe('homepage content', function() {
     cy.get('[data-testid="loo:5b60c18c496d4e000532a27d"]');
     cy.get('[data-testid="loo:574db13bdb14a11000cb4766"]');
   });
+
+  it('click on the first loo and get taken to the correct add/edit page', () => {
+    cy.get('[data-testid="loo:592456d4d7ffa80011a39c31"]').click();
+    cy.url().should('include', '/592456d4d7ffa80011a39c31');
+  });
 });
