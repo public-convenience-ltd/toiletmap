9ebfead4906e5f85b43b60bf07d1b804e7ad0530
Rupert Redington <rupert@neontribe.co.uk>
2019-11-22 14:39:40 +0000

    feat: track client-side pageViews

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -77,10 +77,12 @@ sagaMiddleware.run(mapControlsSaga);
 // Set a function to be called on location change
 history.listen(function(location) {
   // If we havn't opted in, we shouldn't have digitalData on window
-  if (window.hasOwnProperty('digitalData')) {
+  if (window.hasOwnProperty('digitalData') && window.hasOwnProperty('s')) {
     // does not include
     window.digitalData.page.pageInfo.pageName = `${document.title}`;
-    window.digitalData.page.pageInfo.destinationURL = `${window.location.href}`;
+    window.digitalData.page.attributes.contentType = '200';
+    // Fire a track (I know...)
+    window.s.t();
   }
 });
 
