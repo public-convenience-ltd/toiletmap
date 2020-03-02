5d6c8dc3849cc7d7c0887e6b02d28f7e9ec64a77
Rupert Redington <rupert@neontribe.co.uk>
2020-03-02 08:01:54 +0000

    fix: more api cleanup

--- packages/explorer/src/setupProxy.js
+++ packages/explorer/src/setupProxy.js
@@ -2,14 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
-    createProxyMiddleware({
-      target: process.env.PROXY || 'https://gbptm-stage.herokuapp.com',
-      //secure: false,
-      changeOrigin: true,
-    })
-  );
-  app.use(
     '/graphql',
     createProxyMiddleware({
       target: process.env.PROXY || 'https://gbptm-stage.herokuapp.com',
