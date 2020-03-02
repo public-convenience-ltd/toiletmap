e93aa76fb48532628c7d8ee7974e6899457aa336
Rupert Redington <rupert@neontribe.co.uk>
2020-03-02 08:01:54 +0000

    fix: more api cleanup

--- packages/ui/src/setupProxy.js
+++ packages/ui/src/setupProxy.js
@@ -27,14 +27,6 @@ module.exports = function(app) {
   });
 
   app.use(
-    '/api',
-    createProxyMiddleware({
-      target: process.env.PROXY || 'https://gbptm-stage.herokuapp.com',
-      //secure: false,
-      changeOrigin: true,
-    })
-  );
-  app.use(
     '/graphql',
     createProxyMiddleware({
       target: process.env.PROXY || 'https://gbptm-stage.herokuapp.com',
