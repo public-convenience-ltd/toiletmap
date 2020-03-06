83c63fe10cf9d49ddd4c57a34d2df9b8433f2751
James Thistlewood <jamesthistlewood@gmail.com>
2020-03-06 18:17:00 +0000

    fix: add domestos use our loos logo
    resolves #428

--- packages/ui/src/components/Header.js
+++ packages/ui/src/components/Header.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { HashLink } from 'react-router-hash-link';
 
+import config from '../config';
+
 import styles from './css/header.module.css';
 import domestosLogo from '../images/domestos_logo3.png';
+import domestosUseLoos from '../images/domestos_use_our_loos_logo.png';
 
 import Logo from '../components/Logo';
 
@@ -11,19 +14,30 @@ class Header extends Component {
     return (
       <header className={styles.header}>
         <Logo />
-        <HashLink
-          to="/use-our-loos"
-          title="Domestos - Use Our Loos Campaign"
-          className={styles.sponsorWrapper}
-          scroll={el => el.scrollIntoView(true)}
-        >
-          <span className={styles.sponsorTitle}>proudly sponsored by</span>
-          <img
-            src={domestosLogo}
-            alt="Domestos"
-            className={styles.sponsorLogo}
-          />
-        </HashLink>
+        {config.shouldShowSponsor() && (
+          <HashLink
+            to="/use-our-loos"
+            title="Domestos - Use Our Loos Campaign"
+            className={styles.sponsorWrapper}
+            scroll={el => el.scrollIntoView(true)}
+          >
+            <img
+              src={domestosUseLoos}
+              alt="Domestos use our loos logo"
+              className={styles.sponsorLogoLeft}
+            />
+            <span className={styles.sponsorTitle}>
+              proudly
+              <br />
+              sponsored by
+            </span>
+            <img
+              src={domestosLogo}
+              alt="Domestos logo"
+              className={styles.sponsorLogoRight}
+            />
+          </HashLink>
+        )}
       </header>
     );
   }
