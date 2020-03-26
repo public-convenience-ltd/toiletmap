9ade5cfc20036048635cead841bf64865fb8eb18
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2020-03-26 09:26:53 +0000

    feat: add apollo link error

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -39,11 +39,13 @@ import config from './config';
 import {
   ApolloClient,
   ApolloProvider,
+  ApolloLink,
   HttpLink,
   InMemoryCache,
 } from '@apollo/client';
 
-import { setContext } from 'apollo-link-context';
+import { onError } from '@apollo/link-error';
+import { setContext } from '@apollo/link-context';
 
 import { version } from '../package.json';
 import { gql } from 'graphql.macro';
@@ -65,6 +67,16 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
+
 const cache = new InMemoryCache({
   typePolicies: {
     Loo: {
@@ -80,7 +92,7 @@ const cache = new InMemoryCache({
 const client = new ApolloClient({
   name: '@toiletmap/ui',
   version: version,
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([errorLink, authLink, httpLink]),
   connectToDevTools: true,
   cache,
   ...localSchema,
