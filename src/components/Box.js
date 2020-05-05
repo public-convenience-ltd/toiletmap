f647cf6f9c7a77abcc754c99097962a6944e62d0
Matt Layton <mattlayt@gmail.com>
2020-05-05 11:39:58 +0100

    feat: add Box component

---
+++ src/components/Box.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import { compose, space, color, layout, flexbox } from 'styled-system';
+import { createPropTypes } from '@styled-system/prop-types';
+
+// https://styled-system.com/guides/build-a-box
+const Box = styled.div`
+  ${compose(space, color, layout, flexbox)}
+
+  // ensures the Box can shrink below its minimum content size when used as a flex item
+  min-width: 0;
+`;
+
+Box.propTypes = {
+  ...createPropTypes(space.propNames),
+  ...createPropTypes(color.propNames),
+  ...createPropTypes(layout.propNames),
+  ...createPropTypes(flexbox.propNames),
+};
+
+export default Box;
