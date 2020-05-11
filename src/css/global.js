c2e1a4667d9d4e1b47d4472f64cc6915982877a8
Matt Layton <mattlayt@gmail.com>
2020-05-11 10:55:53 +0100

    refactor: remove global.js

--- src/css/global.js
+++
@@ -1,7 +0,0 @@
-// Global CSS
-import 'normalize.css/normalize.css';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
-import 'leaflet-loading/src/Control.Loading.css';
-
-import './global.css';
