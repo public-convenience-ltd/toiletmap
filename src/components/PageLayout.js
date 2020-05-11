850c65f402f8db5cf269bfc7f0b2461809817e82
Matt Layton <mattlayt@gmail.com>
2020-05-11 10:12:14 +0100

    feat: add theme Provider

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'emotion-theming';
 
 import MediaQuery from 'react-responsive';
 
@@ -9,6 +10,7 @@ import Footer from './Footer';
 
 import layout from './css/layout.module.css';
 
+import theme from '../theme';
 import config from '../config';
 import { TRACKING_STORAGE_KEY } from './Tracking';
 import TrackingPreferences from './Tracking/TrackingPreferences';
@@ -27,35 +29,43 @@ const PageLayout = (props) => {
   }, [mainRef, isCookieSettingsOpen]);
 
   return (
-    <div className={layout.appContainer}>
-      <div className={layout.mainContainer}>
-        <div className={layout.main}>
-          <Header />
+    <ThemeProvider theme={theme}>
+      <div className={layout.appContainer}>
+        <div className={layout.mainContainer}>
+          <div className={layout.main}>
+            <Header />
 
-          <main ref={mainRef} className={layout.content}>
-            {props.main && React.cloneElement(props.main, props)}
-          </main>
+            <main ref={mainRef} className={layout.content}>
+              {props.main && React.cloneElement(props.main, props)}
+            </main>
 
-          <Footer
-            onCookieBoxButtonClick={() =>
-              setIsCookieSettingsOpen(!isCookieSettingsOpen)
-            }
-            isCookieSettingsOpen={isCookieSettingsOpen}
-          />
+            <Footer
+              onCookieBoxButtonClick={() =>
+                setIsCookieSettingsOpen(!isCookieSettingsOpen)
+              }
+              isCookieSettingsOpen={isCookieSettingsOpen}
+            />
+          </div>
         </div>
+
+        <MediaQuery minWidth={config.viewport.mobile}>
+          <Box
+            as="aside"
+            data-testid="mainMap"
+            display="flex"
+            flex="1"
+            bg="#fff"
+          >
+            {props.map && React.cloneElement(props.map, props)}
+          </Box>
+        </MediaQuery>
+
+        <TrackingPreferences
+          isOpen={isCookieSettingsOpen}
+          onClose={() => setIsCookieSettingsOpen(false)}
+        />
       </div>
-
-      <MediaQuery minWidth={config.viewport.mobile}>
-        <Box as="aside" data-testid="mainMap" display="flex" flex="1" bg="#fff">
-          {props.map && React.cloneElement(props.map, props)}
-        </Box>
-      </MediaQuery>
-
-      <TrackingPreferences
-        isOpen={isCookieSettingsOpen}
-        onClose={() => setIsCookieSettingsOpen(false)}
-      />
-    </div>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  colors: {
+    primary: 'hotpink',
+  },
+};
+
+export default theme;
