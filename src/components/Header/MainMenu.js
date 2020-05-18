5c38b13367d60506bc50e79684dd91da0ab0b2e3
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2020-05-18 18:49:40 +0100

    feat: add extra pages

--- src/components/Header/MainMenu.js
+++ src/components/Header/MainMenu.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import styled from '@emotion/styled';
 
 import Box from '../Box';
 import Text from '../Text';
 import { Media } from '../Media';
+
+const StyledNavLink = styled(NavLink)`
+  // active class is added by NavLink component
+  &.active {
+    color: ${(props) => props.theme.colors.tertiary};
+  }
+`;
 
 // Todo: Contact link
 const MainMenu = ({ children }) => (
@@ -28,20 +36,22 @@ const MainMenu = ({ children }) => (
         flexGrow={1}
       >
         <Box as="li" ml={[0, 4]}>
-          <Link to="/">Find Loo</Link>
+          <StyledNavLink to="/" exact>
+            Find Loo
+          </StyledNavLink>
         </Box>
         <Box as="li" mt={[3, 0]} ml={[0, 4]}>
-          <Link to="/report">Add Loo</Link>
+          <StyledNavLink to="/report">Add Loo</StyledNavLink>
         </Box>
 
         <Box as="li" mt={['auto', 0]} ml={[0, 'auto']}>
-          <Link to="/about">About</Link>
+          <StyledNavLink to="/about">About</StyledNavLink>
         </Box>
         <Box as="li" mt={[3, 0]} ml={[0, 4]}>
-          <Link to="/use-our-loos">Our Sponsor</Link>
+          <StyledNavLink to="/use-our-loos">Our Sponsor</StyledNavLink>
         </Box>
         <Box as="li" mt={[3, 0]} mb={['auto', 0]} ml={[0, 4]}>
-          <Link to="">Contact</Link>
+          <StyledNavLink to="/contact">Contact</StyledNavLink>
         </Box>
       </Box>
 
