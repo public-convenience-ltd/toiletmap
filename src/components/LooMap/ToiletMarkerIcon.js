b6db779584acc1aa3914fa68d517e655a078b3b2
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2020-05-18 09:19:44 +0100

    feat: add map marker stroke

--- src/components/LooMap/ToiletMarkerIcon.js
+++ src/components/LooMap/ToiletMarkerIcon.js
@@ -12,20 +12,15 @@ const ToiletMarkerIcon = L.DivIcon.extend({
   initialize: function (options) {
     this.options = {
       ...this.options,
-      html: options.highlight
-        ? `
-        <div data-testid="toiletMarker:${options.toiletId}">
-          <svg  viewBox="0 0 20 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.47632 0 0 4.47529 0 10C0 19.5501 10 32 10 32C10 32 20 19.5501 20 10C20 4.47529 15.5237 0 10 0Z" fill="#ED3D63"/>
-            <path d="M10 4L11.7634 7.57295L15.7063 8.1459L12.8532 10.9271L13.5267 14.8541L10 13L6.47329 14.8541L7.14683 10.9271L4.29366 8.1459L8.23664 7.57295L10 4Z" fill="white"/>
-          </svg>
-        </div>
-      `
-        : `
+      html: `
         <div data-testid="toiletMarker:${options.toiletId}">
           <svg viewBox="0 0 20 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.47632 0 0 4.47529 0 10C0 19.5501 10 32 10 32C10 32 20 19.5501 20 10C20 4.47529 15.5237 0 10 0Z" fill="#ED3D63" />
-            <circle cx="10" cy="10" r="5" fill="white"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M10 0C4.47632 0 0 4.47529 0 10C0 19.5501 10 32 10 32C10 32 20 19.5501 20 10C20 4.47529 15.5237 0 10 0Z" fill="#ED3D63" stroke="white"/>
+            ${
+              options.highlight
+                ? '<path d="M10 4L11.7634 7.57295L15.7063 8.1459L12.8532 10.9271L13.5267 14.8541L10 13L6.47329 14.8541L7.14683 10.9271L4.29366 8.1459L8.23664 7.57295L10 4Z" fill="white"/>'
+                : '<circle cx="10" cy="10" r="5" fill="white"/>'
+            }
           </svg>
         </div>
       `,
