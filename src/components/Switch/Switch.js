65cee7a328db61134aca52755a5339e7cca8414c
Oliver Barnwell <ob6160@users.noreply.github.com>
2021-05-01 15:43:41 +0100

    fix: switch elements after react form update (#1072)

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -13,39 +13,47 @@ const Inner = styled(Box)`
   transition: left 0.2s ease;
 `;
 
-const Switch = ({
-  checked,
-  onClick = Function.prototype,
-  onChange = Function.prototype,
-  ...props
-}) => (
-  <Box
-    as="button"
-    type="button"
-    role="switch"
-    {...props}
-    aria-checked={checked}
-    position="relative"
-    bg={checked ? 'tertiary' : 'primary'}
-    height={HEIGHT}
-    width={WIDTH}
-    borderRadius={18}
-    onClick={() => {
-      onClick();
-      onChange(!checked);
-    }}
-  >
-    <Inner
-      as="span"
-      position="absolute"
-      top="1px"
-      left={checked ? `calc(100% - ${LENGTH}px - ${OFFSET}px)` : `${OFFSET}px`}
-      height={LENGTH}
-      width={LENGTH}
-      borderRadius="50%"
-      bg="white"
-    />
-  </Box>
+const Switch = React.forwardRef(
+  (
+    {
+      checked,
+      onClick = Function.prototype,
+      onChange = Function.prototype,
+      ...props
+    },
+    ref
+  ) => (
+    <Box
+      as="button"
+      type="button"
+      role="switch"
+      {...props}
+      ref={ref}
+      aria-checked={checked}
+      position="relative"
+      bg={checked ? 'tertiary' : 'primary'}
+      height={HEIGHT}
+      width={WIDTH}
+      borderRadius={18}
+      onClick={() => {
+        onClick();
+        onChange(!checked);
+      }}
+    >
+      <Inner
+        as="span"
+        position="absolute"
+        top="1px"
+        left={
+          checked ? `calc(100% - ${LENGTH}px - ${OFFSET}px)` : `${OFFSET}px`
+        }
+        height={LENGTH}
+        width={LENGTH}
+        borderRadius="50%"
+        bg="white"
+      />
+    </Box>
+  )
 );
 
 Switch.propTypes = {
