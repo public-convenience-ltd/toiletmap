be38f8be0d6238e1033472b2335f2ef0d863cc40
Oliver Barnwell <barnwelloliver@gmail.com>
2021-06-27 17:07:59 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
     webpack: (config, { isServer }) => {
       if (!isServer) {
         config.resolve.fallback.fs = false;
-      }  
+      }
       return config;
     },
+
   }
