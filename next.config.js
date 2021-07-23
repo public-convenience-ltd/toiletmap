8c56ce49e1648b4b40e9eebce2c284b3724cca17
Oliver Barnwell <barnwelloliver@gmail.com>
2021-07-23 15:13:06 +0100

    feat: switch to esbuild

--- next.config.js
+++ next.config.js
@@ -1,11 +1,47 @@
-module.exports = {
-    webpack: (config, { isServer }) => {
-      config.module.rules.push({
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: 'graphql-tag/loader',
-      });
-      return config;
-    },
 
-  }
+
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
+function useEsbuildMinify(config, options) {
+  const { minimizer } = config.optimization;
+  const terserIndex = minimizer.findIndex(
+    minifier => minifier.constructor.name === 'TerserPlugin',
+  );
+
+  minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+}
+
+function useEsbuildLoader(config, options) {
+  const { rules } = config.module;
+  const babelIndex = rules.findIndex(rule => rule.test.test('.js'));
+
+  rules[babelIndex].use = {
+    loader: 'esbuild-loader',
+    options,
+  };
+}
+
+module.exports = {
+  webpack: (config, { webpack }) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+    );
+
+    useEsbuildMinify(config);
+    useEsbuildLoader(config, {
+      // Specify `tsx` if you're using TypeScript
+      loader: 'tsx',
+      target: 'es2017',
+    });
+
+    return config;
+  },
+};
