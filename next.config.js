d8de1b40d8881b4c8d6207d5bce074587b37effb
Oliver Barnwell <barnwelloliver@gmail.com>
2021-08-02 18:22:50 +0100

    put build output back

--- next.config.js
+++ next.config.js
@@ -11,7 +11,6 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  distDir: 'build',
   webpack: (config, { isServer }) => {
     // config.module.rules.push({
     //   test: /\.(graphql|gql)$/,
