a2af6c3321615e2889056d872cd6cc322bae87bf
Oliver Barnwell <barnwelloliver@gmail.com>
2021-08-03 18:02:12 +0100

    fix so we use one typedef file for both api impls

--- src/api/typeDefs.ts
+++ src/api/typeDefs.ts
@@ -1,5 +1,8 @@
-import { gql } from 'apollo-server-micro'
-const typeDefs = gql(
-  fs.readFileSync(path.join(__dirname, 'typeDefs.graphql'), 'utf-8')
+import { gql } from 'apollo-server-micro';
+
+import fs from 'fs';
+import path from 'path';
+
+export default gql(
+  fs.readFileSync(path.join(process.cwd(), './src/api/typeDefs.graphql'), 'utf-8')
 );
-export default gql(typeDefs);
