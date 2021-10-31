a12650bedef249056c173678fd00325f7443bb35
Oliver Barnwell <barnwelloliver@gmail.com>
2021-10-31 02:04:35 +0000

    feat: hooray, filtering works

--- src/components/LooMap/Markers.tsx
+++ src/components/LooMap/Markers.tsx
@@ -1,25 +1,17 @@
-import React, {
-  useMemo,
-  useCallback,
-  useState,
-  useEffect,
-  useRef,
-  forwardRef,
-} from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { useRouter } from 'next/router';
-import {
-  Loo,
-  useMinimumViableLooResponseQuery,
-} from '../../api-client/graphql';
-import { Marker, useMapEvents, useMap, MarkerProps } from 'react-leaflet';
+import { Loo } from '../../api-client/graphql';
+import { Marker } from 'react-leaflet';
 import ToiletMarkerIcon from './ToiletMarkerIcon';
-import MarkerClusterGroup from 'react-leaflet-markercluster';
-import config from '../../config';
+import * as L from 'leaflet';
+import 'leaflet.markercluster';
+import { useMap } from 'react-leaflet';
 const KEY_ENTER = 13;
 
-// const MarkerCustom: React.FC<MarkerProps> = (props) => {
-//   return <div id={props.<Marker {...props} />;
-// };
+const mcg = L.markerClusterGroup({
+  chunkedLoading: true,
+  showCoverageOnHover: false,
+});
 
 const Markers = ({
   focus,
@@ -32,127 +24,68 @@ const Markers = ({
 }) => {
   const router = useRouter();
 
+  const toiletMarkerIcon = useCallback(
+    (toilet, focusId) =>
+      new ToiletMarkerIcon({
+        isHighlighted: toilet.id === focusId,
+        toiletId: toilet.id,
+        isUseOurLoosCampaign: toilet.campaignUOL,
+      }),
+    []
+  );
+
   const filteredLooGroups = useMemo(() => {
     const filters = [
-      {
-        id: 'noPayment',
-        label: 'Free',
-      },
-      {
-        id: 'babyChange',
-        label: 'Baby Changing',
-      },
-      {
-        id: 'accessible',
-        label: 'Accessible',
-      },
-      {
-        id: 'allGender',
-        label: 'Gender Neutral',
-      },
-      {
-        id: 'radar',
-        label: 'Radar Key',
-      },
-      {
-        id: 'automatic',
-        label: 'Automatic',
-      },
+      'noPayment',
+
+      'babyChange',
+
+      'accessible',
+
+      'allGender',
+
+      'radar',
+
+      'automatic',
     ];
-    const groups = {};
-    filters.forEach((group) => {
-      const filteredRealLooGroup = loos.filter((loo) => !loo[group.id]);
-      groups[group.id] = filteredRealLooGroup.map((toilet) => {
-        return (
-          <Marker
-            pane={group.id}
-            key={toilet.id}
-            position={toilet.location}
-            zIndexOffset={toilet.id === focus?.id ? 1000 : 0}
-            icon={
-              new ToiletMarkerIcon({
-                isHighlighted: toilet.id === focus?.id,
-                toiletId: toilet.id,
-                isUseOurLoosCampaign: toilet.campaignUOL,
-              })
-            }
-            alt={toilet.name || 'Unnamed toilet'}
-            eventHandlers={{
-              click: () => {
-                router.push(`/loos/${toilet.id}`);
-              },
-              keydown: (event: { originalEvent: { keyCode: number } }) => {
-                if (event.originalEvent.keyCode === KEY_ENTER) {
-                  router.push(`/loos/${toilet.id}`);
-                }
-              },
-            }}
-            keyboard={false}
-          />
-        );
-      });
+
+    const totalLoos = loos.filter((loo) => {
+      for (const filter of filters) {
+        if (flobs[filter]) {
+          if (loo[filter] === true) {
+            return false;
+          }
+        }
+      }
+      return true;
     });
 
-    return groups;
-  }, [loos, focus?.id, router]);
-  return (
-    <>
-      <MarkerClusterGroup
-        animateAddingMarkers={false}
-        removeOutsideVisibleBounds
-        chunkedLoading
-        maxClusterRadius={200}
-        clusterPane={'noPayment'}
-      >
-        {filteredLooGroups['noPayment']}
-      </MarkerClusterGroup>
-      <MarkerClusterGroup
-        animateAddingMarkers={false}
-        removeOutsideVisibleBounds
-        chunkedLoading
-        maxClusterRadius={200}
-        clusterPane={'babyChange'}
-      >
-        {filteredLooGroups['babyChange']}
-      </MarkerClusterGroup>
-      <MarkerClusterGroup
-        animateAddingMarkers={false}
-        removeOutsideVisibleBounds
-        chunkedLoading
-        maxClusterRadius={200}
-        clusterPane={'accessible'}
-      >
-        {filteredLooGroups['accessible']}
-      </MarkerClusterGroup>
-      <MarkerClusterGroup
-        animateAddingMarkers={false}
-        removeOutsideVisibleBounds
-        chunkedLoading
-        maxClusterRadius={200}
-        clusterPane={'allGender'}
-      >
-        {filteredLooGroups['allGender']}
-      </MarkerClusterGroup>
-      <MarkerClusterGroup
-        animateAddingMarkers={false}
-        removeOutsideVisibleBounds
-        chunkedLoading
-        maxClusterRadius={200}
-        clusterPane={'radar'}
-      >
-        {filteredLooGroups['radar']}
-      </MarkerClusterGroup>
-      <MarkerClusterGroup
-        animateAddingMarkers={false}
-        removeOutsideVisibleBounds
-        chunkedLoading
-        maxClusterRadius={200}
-        clusterPane={'automatic'}
-      >
-        {filteredLooGroups['automatic']}
-      </MarkerClusterGroup>
-    </>
-  );
+    return totalLoos.map((toilet) =>
+      L.marker(new L.LatLng(toilet.location.lat, toilet.location.lng), {
+        zIndexOffset: toilet.id === focus?.id ? 1000 : 0,
+        icon: toiletMarkerIcon(toilet, focus?.id),
+        alt: toilet.name || 'Unnamed toilet',
+        keyboard: false,
+      })
+        .on('click', () => {
+          router.push(`/loos/${toilet.id}`);
+        })
+        .on('keydown', (event: { originalEvent: { keyCode: number } }) => {
+          if (event.originalEvent.keyCode === KEY_ENTER) {
+            router.push(`/loos/${toilet.id}`);
+          }
+        })
+    );
+  }, [loos, flobs, focus?.id, toiletMarkerIcon, router]);
+
+  const map = useMap();
+  useEffect(() => {
+    mcg.clearLayers();
+    mcg.addLayers(filteredLooGroups);
+    map.addLayer(mcg);
+  }, [map, filteredLooGroups]);
+
+  return null;
 };
 
 export default Markers;
