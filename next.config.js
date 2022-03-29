45cded009b01cbba3cf2f52f4eaa997f40adb788
Oliver Barnwell <barnwelloliver@gmail.com>
2022-03-29 20:59:30 +0100

    chore: try upgrade to react 18

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable functional/immutable-data */
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      // Map lng-lat routes to a single page
+      {
+        source: '/map/:lng/:lat',
+        destination: '/map',
+      },
+    ];
+  },
+  webpack: (config, {}) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.(graphql|gql)$/,
+            exclude: /node_modules/,
+            loader: 'graphql-tag/loader',
+          },
+        ],
+      },
+    };
+  },
+};
