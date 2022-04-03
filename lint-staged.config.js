e63f8028867af0ce0d2d67ab8979e4c7eec7574d
Oliver Barnwell <barnwelloliver@gmail.com>
2022-04-03 17:04:33 +0100

    feat: get the index story fully working

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line functional/immutable-data
 module.exports = {
-  // '*.{js,ts,tsx}': ['prettier --write', 'eslint --max-warnings=0'],
+  '*.{js,ts,tsx}': ['prettier --write', 'eslint --max-warnings=0'],
   //'**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
   '*.{json,md,yml}': ['prettier --write'],
 };
