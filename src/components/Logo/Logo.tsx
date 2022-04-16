782e80a913d5e0fe6cc7f914a6f5bdb491ee4293
Oliver Barnwell <barnwelloliver@gmail.com>
2022-04-16 16:44:02 +0100

    fix: fix layout of about and contact pages

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import Box from '../Box';
 import logo from '../../../public/logo.svg';
 
 const Logo = React.forwardRef<HTMLDivElement>((props, ref) => (
-  <Box width="150px" ref={ref}>
+  <Box width="154px" ref={ref}>
     <Image
       src={logo}
       alt="The Great British Toilet Map"
