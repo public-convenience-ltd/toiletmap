5b34b44861dce98337916ef567293da0ededaeec
Oliver Barnwell <barnwelloliver@gmail.com>
2022-04-20 19:47:06 +0100

    feat: add cypress base files and first working test

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,8 +1,6 @@
 describe('Home page tests', () => {
-  const baseUrl = 'http://localhost:4444';
-
   it('is correctly titled', () => {
-    cy.visit(baseUrl);
-    cy.title().should('equal', 'The Great British Toilet Map: Find Toilet');
+    cy.visit('/');
+    cy.title().should('equal', 'Toilet Map: Home');
   });
 });
