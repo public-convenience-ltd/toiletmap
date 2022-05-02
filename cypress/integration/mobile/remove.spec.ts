1a21c52cc55470cc46f4755d9a98073d681b03d0
Oliver Barnwell <barnwelloliver@gmail.com>
2022-05-02 22:31:38 +0100

    feat: add edit tests for mobile and desktop

---
+++ cypress/integration/mobile/remove.spec.ts
@@ -0,0 +1,7 @@
+// describe('Remove page tests', () => {
+//   context('Mobile', () => {
+//     it('should remove a given valid toilet id', () => {});
+
+//     it('should handle when the given toilet id is invalid', () => {});
+//   });
+// });
--- src/pages/404.page.tsx
+++ src/pages/404.page.tsx
@@ -4,37 +4,40 @@ import Spacer from '../components/Spacer';
 import Text from '../components/Text';
 import Button from '../components/Button';
 
-const NotFound = () => (
-  <Box my={5}>
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      height="100%"
-      width="100%"
-      p={3}
-    >
-      <div>
-        <Text fontSize={20} fontWeight="bold">
-          <h1>Page not found</h1>
-        </Text>
+const NotFound = ({ children }) => (
+  <>
+    {children}
+    <Box my={5}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+        width="100%"
+        p={3}
+      >
+        <div>
+          <Text fontSize={20} fontWeight="bold">
+            <h1>Page not found</h1>
+          </Text>
 
-        <Spacer mt={3} />
+          <Spacer mt={3} />
 
-        <p>
-          This could be because the page has moved, or you typed an address
-          wrong.
-        </p>
+          <p>
+            This could be because the page has moved, or you typed an address
+            wrong.
+          </p>
 
-        <Spacer mt={4} />
+          <Spacer mt={4} />
 
-        <Button as={'a'} variant="link" href="/">
-          Take me back home!
-        </Button>
-      </div>
+          <Button as={'a'} variant="link" href="/">
+            Take me back home!
+          </Button>
+        </div>
+      </Box>
     </Box>
-  </Box>
+  </>
 );
 
 export default NotFound;
