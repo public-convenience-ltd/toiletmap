11db24f70073b02791608fa08f54a61bf5f594de
Oliver Barnwell <barnwelloliver@gmail.com>
2022-05-02 19:00:46 +0100

    chore: add attribution in for OSM contributors

--- src/components/LooMap/LooMap.tsx
+++ src/components/LooMap/LooMap.tsx
@@ -279,6 +279,7 @@ const LooMap: React.FC<LooMapProps> = ({
         `}
       >
         <TileLayer
+          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           minZoom={minZoom}
           maxZoom={maxZoom}
