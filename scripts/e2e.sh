25113c860d94b98a4da1d9092b7be32b74bf6d37
Oliver Barnwell <barnwelloliver@gmail.com>
2022-05-02 12:36:25 +0100

    feat: add dev mode cypress and improve script for headless

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -10,5 +10,28 @@ source .env
 
 NODE_ENV=test
 
+
+yarn startMemoryMongo & yarn wait-on:memoryMongo
+
+while getopts 'lha:' OPTION; do
+  case "$OPTION" in
+    h)
+      echo "Running cypress tests in headless mode"
+      yarn build
+      yarn start-server-and-test start http://localhost:3000 cypress:headless
+      exit 1
+      ;;
+    d)
+      echo "Running cypress tests in development mode"
+      yarn dev
+      yarn start-server-and-test start http://localhost:3000 cypress
+    ?)
+      echo "script usage: $(basename \$0) [-h]" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
 yarn build
 yarn start-server-and-test start http://localhost:3000 cypress
