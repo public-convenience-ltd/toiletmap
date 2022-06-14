856ff8bc8ae12d99c7104b2f29fad342ff5395ac
Rupert Redington <rupert@lola.tech>
2022-06-14 16:52:30 +0100

    fix: api proxy

--- src/graphql/fetcher.js
+++ src/graphql/fetcher.js
@@ -3,7 +3,7 @@ import { GraphQLClient } from 'graphql-request';
 
 import { isAuthenticated, getAccessToken } from '../Auth';
 
-const API_ENDPOINT = 'https://toiletmap.org.uk/api';
+const API_ENDPOINT = '/api';
 
 const fetcher = (query, variables) => {
   const graphQLClient = new GraphQLClient(API_ENDPOINT);
