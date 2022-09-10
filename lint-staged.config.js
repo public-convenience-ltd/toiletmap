ac5c25a6e25f9e45b9e729d4d43bf98407f5c0bc
Oliver Barnwell <barnwelloliver@gmail.com>
2022-09-10 20:50:33 +0100

    remove unnecessary comment from lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line functional/immutable-data
 module.exports = {
   '*.{js,ts,tsx}': ['prettier --write', 'eslint --max-warnings=0'],
-  //'**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
   '*.{json,md,yml}': ['prettier --write'],
 };
