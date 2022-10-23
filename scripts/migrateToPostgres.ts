7bb06960aed42179e1d7fbec15239b59a913eb50
Oliver Barnwell <barnwelloliver@gmail.com>
2022-10-23 23:27:13 +0100

    feat: add a loading indicator and start work on area support

--- scripts/migrateToPostgres.ts
+++ scripts/migrateToPostgres.ts
@@ -1,101 +1,136 @@
 import { PrismaClient } from '../generated/prisma-client-js';
 import { PrismaClient as PrismaClientMongo } from '../generated/schemaMongo';
+import { SingleBar } from 'cli-progress';
 
 (async () => {
   const mongoPrisma = new PrismaClientMongo();
   mongoPrisma.$connect();
-  const allMongoLoos = await mongoPrisma.newloos.findMany();
 
-  const psqlPrisma = new PrismaClient();
-  psqlPrisma.$connect();
+  // const allMongoAreas = await mongoPrisma.areas.findMany();
 
-  for (const loo of allMongoLoos) {
-    const {
-      accessible,
-      active,
-      // area,
-      attended,
-      automatic,
-      babyChange,
-      men,
-      noPayment,
-      notes,
-      openingTimes,
-      paymentDetails,
-      radar,
-      removalReason,
-      urinalOnly,
-      allGender,
-      children,
-      women,
-      name,
-      // verifiedAt
-    } = loo.properties;
+  const upsertLoos = async () => {
+    console.log('Fetching data from Mongo...');
+    const allMongoLoos = await mongoPrisma.newloos.findMany();
+    const allMongoReports = await mongoPrisma.newreports.findMany();
+    const mappedMongoReports = {};
+    for (const report of allMongoReports) {
+      mappedMongoReports[report.id] = report;
+    }
 
-    const resolvedReports = await mongoPrisma.newreports.findMany({
-      where: { id: { in: loo.reports } },
+    const psqlPrisma = new PrismaClient();
+    psqlPrisma.$connect();
+
+    console.log('Upserting loos...');
+
+    const bar = new SingleBar({
+      stopOnComplete: true,
+      etaBuffer: 30,
+      barCompleteChar: '✨',
     });
 
-    const upsertLoo = psqlPrisma.toilets.upsert({
-      where: { legacy_id: loo.id },
-      create: {
+    bar.start(allMongoLoos.length, 0);
+
+    let index = 0;
+
+    for (const loo of allMongoLoos) {
+      const {
+        accessible,
+        active,
+        // area,
+        attended,
+        automatic,
+        babyChange,
+        men,
+        noPayment,
+        notes,
+        openingTimes,
+        paymentDetails,
+        radar,
+        removalReason,
+        urinalOnly,
+        allGender,
+        children,
+        women,
         name,
-        legacy_id: loo.id,
-        accessible,
-        active,
-        attended,
-        automatic,
-        babyChange,
-        men,
-        noPayment,
-        notes,
-        paymentDetails,
-        radar,
-        removalReason,
-        women,
-        created_at: loo.createdAt,
-        updated_at: loo.updatedAt,
-        contributors: loo.contributors,
-        urinalOnly,
-        allGender,
-        children,
-        openingTimes: (openingTimes ?? undefined)?.flat(),
-        reports: resolvedReports,
-      },
-      update: {
-        accessible,
-        active,
-        attended,
-        automatic,
-        babyChange,
-        men,
-        noPayment,
-        notes,
-        paymentDetails,
-        radar,
-        removalReason,
-        women,
-        created_at: loo.createdAt,
-        updated_at: loo.updatedAt,
-        contributors: loo.contributors,
-        urinalOnly,
-        allGender,
-        children,
-        openingTimes: (openingTimes ?? undefined)?.flat(),
-        reports: resolvedReports,
-      },
-    });
+        verifiedAt,
+      } = loo.properties;
 
-    const geometry = loo.properties.geometry;
-    const updateLooGeometry = psqlPrisma.$executeRaw`
-      UPDATE toilets SET
-      geometry = ST_SetSRID(ST_MakePoint(${geometry.coordinates[0]}, ${geometry.coordinates[1]}), 4326)
-      WHERE legacy_id = ${loo.id}
-    `;
+      const resolvedReports = {};
+      for (const report of loo.reports) {
+        resolvedReports[report] = mappedMongoReports[report];
+      }
 
-    const result = await upsertLoo;
-    const updateGeometry = await updateLooGeometry;
+      const upsertLoo = psqlPrisma.toilets.upsert({
+        where: { legacy_id: loo.id },
+        create: {
+          name,
+          legacy_id: loo.id,
+          accessible,
+          active,
+          attended,
+          automatic,
+          babyChange,
+          men,
+          noPayment,
+          notes,
+          paymentDetails,
+          radar,
+          removalReason,
+          women,
+          created_at: loo.createdAt,
+          updated_at: loo.updatedAt,
+          contributors: loo.contributors,
+          urinalOnly,
+          allGender,
+          children,
+          openingTimes: (openingTimes ?? undefined)?.flat(),
+          verifiedAt,
+          reports: resolvedReports,
+        },
+        update: {
+          accessible,
+          active,
+          attended,
+          automatic,
+          babyChange,
+          men,
+          noPayment,
+          notes,
+          paymentDetails,
+          radar,
+          removalReason,
+          women,
+          created_at: loo.createdAt,
+          updated_at: loo.updatedAt,
+          contributors: loo.contributors,
+          urinalOnly,
+          allGender,
+          children,
+          openingTimes: (openingTimes ?? undefined)?.flat(),
+          reports: resolvedReports,
+          verifiedAt,
+        },
+      });
 
-    console.log(result, updateGeometry);
-  }
+      const result = await upsertLoo;
+      if (!result.geohash) {
+        const geometry = loo.properties.geometry;
+        const updateLooGeometry = psqlPrisma.$executeRaw`
+        UPDATE toilets SET
+        geometry = ST_SetSRID(ST_MakePoint(${geometry.coordinates[0]}, ${geometry.coordinates[1]}), 4326)
+        WHERE legacy_id = ${loo.id}
+      `;
+
+        await updateLooGeometry;
+      }
+
+      bar.update(index++);
+    }
+
+    bar.stop();
+
+    console.log('Done.');
+  };
+
+  await upsertLoos();
 })();
