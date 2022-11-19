79dbb357f6c45c53e383d3b3e7e5b1a47a88a16a
Oliver Barnwell <barnwelloliver@gmail.com>
2022-11-19 18:16:07 +0000

    fix the seed

--- supabase/migrations/20221112164242_geography-trigger.sql
+++ supabase/migrations/20221112164242_geography-trigger.sql
@@ -1,30 +1,33 @@
 -- Ensures that a relation is created between loos and the area that they belong to.
 
-CREATE OR REPLACE FUNCTION determine_area_loo_is_within_on_upsert()
+
+CREATE OR REPLACE FUNCTION public.determine_area_loo_is_within_on_upsert()
   RETURNS trigger AS
     $$
     BEGIN
       NEW.area_id := (
         SELECT a.id
-        FROM areas AS a
+        FROM public.areas AS a
         WHERE ST_WITHIN(NEW.geography::geometry, a.geometry::geometry)
         LIMIT 1
       );
       RETURN NEW;
     END;
     $$
-  LANGUAGE 'plpgsql'
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE SECURITY DEFINER PARALLEL UNSAFE
 ;
 
 CREATE TRIGGER loo_area_trigger_update
   BEFORE UPDATE ON public.toilets
   FOR EACH ROW
-  EXECUTE PROCEDURE determine_area_loo_is_within_on_upsert()
+  EXECUTE PROCEDURE public.determine_area_loo_is_within_on_upsert()
 ;
 
 CREATE TRIGGER loo_area_trigger_insert
   BEFORE INSERT ON public.toilets
   FOR EACH ROW
-  EXECUTE PROCEDURE determine_area_loo_is_within_on_upsert()
+  EXECUTE PROCEDURE public.determine_area_loo_is_within_on_upsert()
 ;
 
