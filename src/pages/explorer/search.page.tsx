352aa45546a1eeb22acb651886c2d2656ef61b82
Oliver Barnwell <barnwelloliver@gmail.com>
2023-02-02 00:22:20 +0000

    hacky first pass at a paginated table, it works but it needs query param persistence and filter options

--- src/pages/explorer/search.page.tsx
+++ src/pages/explorer/search.page.tsx
@@ -2,27 +2,57 @@ import * as React from 'react';
 
 import { withApollo } from '../../api-client/withApollo';
 import { TablePaginationUnstyled } from '@mui/base';
+import { useSearchLoosQuery } from '../../api-client/graphql';
 
 const UnstyledTable = () => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rows, setRows] = React.useState<
+    {
+      id: string;
+      name: string;
+      area: string;
+      areaType: string;
+      contributors: string;
+      updatedAt: string;
+    }[]
+  >([]);
+  const [availableRows, setAvailableRows] = React.useState(0);
 
-  const rows = [
-    {
-      name: 'St. Martins Car Park',
-      area: 'Arun',
-      areaType: 'Local Authority',
-      contributors: 'Anonymous',
-      updatedAt: '2021-08-01',
+  const { data, refetch } = useSearchLoosQuery({
+    variables: {
+      filters: { active: true },
+      pagination: { page: page + 1, limit: rowsPerPage },
     },
-    {
-      name: 'Ferring Country Park',
-      area: 'Rother',
-      areaType: 'Local Authority',
-      contributors: 'Anonymous',
-      updatedAt: '2021-02-01',
-    },
-  ];
+  });
+
+  React.useEffect(() => {
+    refetch({
+      filters: { active: true },
+      pagination: { page: page + 1, limit: rowsPerPage },
+    });
+  }, [page, refetch, rowsPerPage]);
+
+  React.useEffect(() => {
+    if (data) {
+      const mapped = data.loos.loos.map((loo) => {
+        if (loo?.area[0] == null) {
+          console.log(loo);
+        }
+        return {
+          id: loo?.id,
+          name: loo?.name ?? 'Unnamed',
+          area: loo?.area[0]?.name,
+          areaType: loo?.area[0]?.type,
+          contributors: 'Anonymous',
+          updatedAt: new Date(loo?.updatedAt).toLocaleDateString(),
+        };
+      });
+
+      setRows(mapped);
+      setAvailableRows(data.loos.total);
+    }
+  }, [data]);
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
@@ -54,30 +84,27 @@ const UnstyledTable = () => {
           </tr>
         </thead>
         <tbody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map((row) => (
-            <tr key={row.name}>
-              <td>{row.name}</td>
-              <td>{row.area}</td>
-              <td>{row.contributors}</td>
-              <td>{row.updatedAt}</td>
+          {rows.map((row) => (
+            <tr key={row?.id}>
+              <td>{row?.name}</td>
+              <td>{row?.area}</td>
+              <td>{row?.contributors}</td>
+              <td>{row?.updatedAt}</td>
             </tr>
           ))}
 
-          {emptyRows > 0 && (
+          {/* {emptyRows > 0 && (
             <tr style={{ height: 34 * emptyRows }}>
               <td colSpan={4} />
             </tr>
-          )}
+          )} */}
         </tbody>
         <tfoot>
           <tr>
             <TablePaginationUnstyled
               rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
               colSpan={4}
-              count={rows.length}
+              count={availableRows}
               rowsPerPage={rowsPerPage}
               page={page}
               slotProps={{
