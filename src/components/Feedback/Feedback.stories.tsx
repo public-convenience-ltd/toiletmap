5e24ae1d920e85ee81a7534404ee7ae820084671
Oliver Barnwell <barnwelloliver@gmail.com>
2023-05-01 14:29:37 +0100

    fix the Feedback story

--- src/components/Feedback/Feedback.stories.tsx
+++ src/components/Feedback/Feedback.stories.tsx
@@ -1,9 +1,23 @@
 import { Meta } from '@storybook/react';
+import { ThemeProvider, createTheme } from '@mui/material';
 import Feedback from './Feedback';
+import theme from '../../theme';
 
 export default {
   name: 'Feedback',
   component: Feedback,
+  decorators: [
+    (Story) => {
+      return (
+        <ThemeProvider
+          // @ts-expect-error -- Expected
+          theme={createTheme(theme)}
+        >
+          <Story />
+        </ThemeProvider>
+      );
+    },
+  ],
 } as Meta;
 
 export const Default = () => <Feedback />;
