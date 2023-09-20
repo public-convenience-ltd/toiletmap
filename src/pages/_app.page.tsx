d025fd6e6efb5571cefdb57e5ddbf671b219502b
roseb.04 <rose@neontribe.co.uk>
2023-09-20 16:20:30 +0100

    refactor: remove fontawesome package and import in _app.page.tsx

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,10 +3,6 @@ import Providers from '../components/Providers';
 import { Analytics } from '@vercel/analytics/react';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import Main from '../components/Main';
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-// eslint-disable-next-line functional/immutable-data
-config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 import { useRouter } from 'next/router';
 import LooMap from '../components/LooMap/LooMapLoader';
 import '../design-system/components/stylesheet.css';
