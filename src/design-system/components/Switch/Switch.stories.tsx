483e072bd4b6c0d382405c531f186ad11bb865da
Steve Burtenshaw <53346+stphnnnn@users.noreply.github.com>
2023-11-29 11:50:26 +0000

    feat: add switch component to design system

---
+++ src/design-system/components/Switch/Switch.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Switch from './Switch';
+import { useState } from 'react';
+
+const meta: Meta<typeof Switch> = {
+  title: 'Design-System/Form Elements/Switch',
+  component: Switch,
+};
+
+export default meta;
+
+const SwitchWithState = () => {
+  const [checked, setChecked] = useState(false);
+  return <Switch onChange={setChecked} checked={checked} />;
+};
+
+export const Primary: StoryObj<typeof Switch> = {
+  render: () => <SwitchWithState />,
+};
