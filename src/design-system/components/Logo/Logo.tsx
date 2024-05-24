8a28d4a0a27f5bec336dbc32c70f61c5a2928a37
rosejbon <rosebonner.04@googlemail.com>
2024-05-24 17:08:00 +0100

    Add logo to design system

---
+++ src/design-system/components/Logo/Logo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '../../../../public/logo.svg';
+
+const Logo = React.forwardRef<HTMLDivElement>((props, ref) => (
+  <div className="logo" ref={ref}>
+    <Image
+      src={logo}
+      alt="The Great British Toilet Map"
+      layout="responsive"
+      unoptimized={!!process.env.STORYBOOK}
+      priority={true}
+      {...props}
+    />
+  </div>
+));
+
+// eslint-disable-next-line functional/immutable-data
+Logo.displayName = 'Logo';
+
+export default Logo;
