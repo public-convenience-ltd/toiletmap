36bf2c4342d984775a0e7e4643bcb55115de9b40
Yash Kumar <90761184+yashkumar2603@users.noreply.github.com>
2024-10-17 14:07:29 +0530

    Fix : Mobile responsiveness issues noticed after Vercel deployment.
    I noticed some issues with the responsiveness on mobile devices after the PR was deployed onto Vercel, I have fixed those.
    The problems were -
    - Table was sticking to the right side
    - Page title and the navbar was sticking to the left side
    Changes -
    - Added Media Query based font size handling and handled horizontal overflow in table

--- src/pages/explorer/index.page.tsx
+++ src/pages/explorer/index.page.tsx
@@ -64,143 +64,183 @@ const Stats = ({ data: { statistics } }: { data: StatisticsQuery }) => {
                 &:focus {
                   box-shadow: 0 0 0 3px ${theme.colors.aqua};
                 } /*Brighter color for Accessibility*/
+                font-size: ${theme
+                  .fontSizes[1]}px; /* Base font size for small screens */
+                ${theme.mediaQueries[0]} {
+                  font-size: ${theme.fontSizes[2]}px; /* For tablets and up */
+                }
+                ${theme.mediaQueries[1]} {
+                  font-size: ${theme.fontSizes[3]}px; /* For desktops */
+                }
               `}
             >
               Go to Search Tool 🔎
             </Link>
           </Text>
           <Spacer mb={2} />
+          <div
+            style={{
+              display: 'inline-block',
+              width: '100%',
+              textAlign: 'center',
+              marginBottom: '1rem',
+            }}
+          >
+            <div style={{ display: 'inline-block', marginRight: '2rem' }}>
+              <Text fontSize={3}>
+                <h3
+                  css={css`
+                    font-size: ${theme.fontSizes[2]}px;
+                    ${theme.mediaQueries[0]} {
+                      font-size: ${theme.fontSizes[3]}px;
+                    }
+                  `}
+                >
+                  Total Toilets: &nbsp;
+                  <Text
+                    as="span"
+                    color={theme.colors.darkGrey}
+                    fontWeight="bold"
+                  >
+                    {statistics.total}
+                  </Text>
+                </h3>
+              </Text>
+            </div>
+
+            <div style={{ display: 'inline-block', marginRight: '2rem' }}>
+              <Text fontSize={3}>
+                <h3
+                  css={css`
+                    font-size: ${theme.fontSizes[2]}px;
+                    ${theme.mediaQueries[0]} {
+                      font-size: ${theme.fontSizes[3]}px;
+                    }
+                  `}
+                >
+                  Active: &nbsp;
+                  <Text as="span" color={theme.colors.green} fontWeight="bold">
+                    {statistics.active}
+                  </Text>
+                </h3>
+              </Text>
+            </div>
+
+            <div style={{ display: 'inline-block' }}>
+              <Text fontSize={3}>
+                <h3
+                  css={css`
+                    font-size: ${theme.fontSizes[2]}px;
+                    ${theme.mediaQueries[0]} {
+                      font-size: ${theme.fontSizes[3]}px;
+                    }
+                  `}
+                >
+                  Removed: &nbsp;
+                  <Text as="span" color={theme.colors.grey} fontWeight="bold">
+                    {statistics.removed}
+                  </Text>
+                </h3>
+              </Text>
+            </div>
+          </div>
+
+          <div
+            style={{
+              width: '100%',
+              maxWidth: '845px', // Same as the container width
+              borderBottom: `2px solid ${theme.colors.primary}`,
+              margin: '0 auto',
+            }}
+          />
+          <Spacer mb={3} />
+          <Text fontSize={5} fontWeight="bold" color={theme.colors.primary}>
+            <h2>Area-wise Breakdown</h2>
+          </Text>
+          <Spacer mb={3} />
+          <table
+            css={css`
+              border-collapse: collapse;
+              width: 100%;
+              box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+              overflow: hidden;
+              /* Responsive font sizes for table cells */
+              td,
+              th {
+                font-size: ${theme
+                  .fontSizes[1]}px; /* Base font size for small screens */
+                ${theme.mediaQueries[0]} {
+                  font-size: ${theme.fontSizes[2]}px; /* For tablets and up */
+                }
+                ${theme.mediaQueries[1]} {
+                  font-size: ${theme.fontSizes[3]}px; /* For desktops */
+                }
+              }
+              tr {
+                &:nth-of-type(odd) {
+                  background-color: ${theme.colors.lightGrey};
+                }
+                &:nth-of-type(even) {
+                  background-color: ${theme.colors.white};
+                }
+              }
+              th {
+                background-color: ${theme.colors.darkGrey};
+                color: ${theme.colors.white};
+                padding: 0.5rem;
+                text-align: left;
+                &:nth-of-type(n + 2) {
+                  text-align: center; /* Center-align for second column onward */
+                }
+              }
+              td {
+                padding: 0.75rem;
+                text-align: left;
+                border-bottom: 1px solid ${theme.colors.grey};
+                &:nth-of-type(n + 2) {
+                  text-align: center; /* Center-align for second column onward */
+                }
+              }
+            `}
+          >
+            <thead>
+              <tr>
+                <th>
+                  <Text fontWeight={'bold'}>Area</Text>
+                </th>
+                <th>
+                  <Text fontWeight={'bold'}>Active Toilets</Text>
+                </th>
+                <th>
+                  <Text fontWeight={'bold'}>Removed Toilets</Text>
+                </th>
+                <th>
+                  <Text fontWeight={'bold'}>Total Toilets</Text>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {statistics.areaToiletCount
+                .sort((a, b) => a.name.localeCompare(b.name))
+                .map((area) => (
+                  <tr key={area.name}>
+                    <td>{area.name}</td>
+                    <td>
+                      <Text color={theme.colors.green}>{area.active}</Text>
+                    </td>
+                    <td>
+                      <Text color={theme.colors.darkGrey}>{area.removed}</Text>
+                    </td>
+                    <td>
+                      <Text color={theme.colors.darkGrey}>
+                        {area.active + area.removed}
+                      </Text>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
-
-        <div
-          style={{
-            display: 'inline-block',
-            width: '100%',
-            textAlign: 'center',
-            marginBottom: '1rem',
-          }}
-        >
-          <div style={{ display: 'inline-block', marginRight: '2rem' }}>
-            <Text fontSize={3}>
-              <h3>
-                Total Toilets: &nbsp;
-                <Text as="span" color={theme.colors.darkGrey} fontWeight="bold">
-                  {statistics.total}
-                </Text>
-              </h3>
-            </Text>
-          </div>
-
-          <div style={{ display: 'inline-block', marginRight: '2rem' }}>
-            <Text fontSize={3}>
-              <h3>
-                Active: &nbsp;
-                <Text as="span" color={theme.colors.green} fontWeight="bold">
-                  {statistics.active}
-                </Text>
-              </h3>
-            </Text>
-          </div>
-
-          <div style={{ display: 'inline-block' }}>
-            <Text fontSize={3}>
-              <h3>
-                Removed: &nbsp;
-                <Text as="span" color={theme.colors.grey} fontWeight="bold">
-                  {statistics.removed}
-                </Text>
-              </h3>
-            </Text>
-          </div>
-        </div>
-
-        <div
-          style={{
-            width: '100%',
-            maxWidth: '845px', // Same as the container width
-            borderBottom: `2px solid ${theme.colors.primary}`,
-            margin: '0 auto',
-          }}
-        />
-
-        <Spacer mb={3} />
-
-        <Text fontSize={5} fontWeight="bold" color={theme.colors.primary}>
-          <h2>Area-wise Breakdown</h2>
-        </Text>
-        <Spacer mb={3} />
-
-        <table
-          css={css`
-            border-collapse: collapse;
-            width: 100%;
-            box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-            tr {
-              &:nth-child(odd) {
-                background-color: ${theme.colors.lightGrey};
-              }
-              &:nth-child(even) {
-                background-color: ${theme.colors.white};
-              }
-            }
-            th {
-              background-color: ${theme.colors.darkGrey};
-              color: ${theme.colors.white};
-              padding: 0.75rem;
-              text-align: left;
-              text-align: left;
-              &:nth-child(n + 2) {
-                text-align: center; /*Center align for second column onward*/
-              }
-            }
-            td {
-              padding: 0.75rem;
-              text-align: left;
-              border-bottom: 1px solid ${theme.colors.grey};
-              text-align: left;
-              &:nth-child(n + 2) {
-                text-align: center; /*Center align for second column onward*/
-              }
-            }
-          `}
-        >
-          <thead>
-            <tr>
-              <th>
-                <Text fontWeight={'bold'}>Area</Text>
-              </th>
-              <th>
-                <Text fontWeight={'bold'}>Active Toilets</Text>
-              </th>
-              <th>
-                <Text fontWeight={'bold'}>Removed Toilets</Text>
-              </th>
-              <th>
-                <Text fontWeight={'bold'}>Total Toilets</Text>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {statistics.areaToiletCount
-              .sort((a, b) => a.name.localeCompare(b.name))
-              .map((area) => (
-                <tr key={area.name}>
-                  <td>{area.name}</td>
-                  <td>
-                    <Text color={theme.colors.green}>{area.active}</Text>
-                  </td>
-                  <td>
-                    <Text color={theme.colors.darkGrey}>{area.removed}</Text>
-                  </td>
-                  <td>
-                    <Text color={theme.colors.darkGrey}>
-                      {area.active + area.removed}
-                    </Text>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
       </Container>
     </Box>
   );
