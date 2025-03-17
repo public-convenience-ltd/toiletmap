b74c7dd1d790e9f9724e094fb193909ac676db9e
Oliver Barnwell <barnwelloliver@gmail.com>
2025-03-17 23:45:36 +0000

    bump auth0 packages to major version 4, update our use of the API to conform with changes listed in the migration guide: https://github.com/auth0/nextjs-auth0/blob/main/V4_MIGRATION_GUIDE.md

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,6 +1,7 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
 export const auth0 = new Auth0Client({
+  appBaseUrl: process.env.APP_BASE_URL,
   authorizationParameters: {
     audience: process.env.AUTH0_AUDIENCE,
     scope: 'openid profile email',
