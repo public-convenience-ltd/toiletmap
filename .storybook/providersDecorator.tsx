c94b1a73c8bae6ffb935b2e0a1710bed1bf19e36
Oliver Barnwell <barnwelloliver@gmail.com>
2025-03-24 18:37:01 +0000

    audience changes

--- .storybook/providersDecorator.tsx
+++ .storybook/providersDecorator.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { DecoratorFn } from '@storybook/react';
 import Providers from '../src/components/Providers';
-import { UserProvider } from '@auth0/nextjs-auth0';
+import { Auth0Provider } from '@auth0/nextjs-auth0';
 
-const providersDecorator: DecoratorFn = (StoryToDecorate, context) => {
+const providersDecorator = (StoryToDecorate, context) => {
   return (
     <Providers>
-      <UserProvider user={context.args?.user}>
+      <Auth0Provider user={context.args?.user}>
         <StoryToDecorate {...context} />
-      </UserProvider>
+      </Auth0Provider>
     </Providers>
   );
 };
