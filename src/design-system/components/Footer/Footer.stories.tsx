48895ba852b00533afb297b3fd550fc7f62592b8
rosejbon <rosebonner.04@googlemail.com>
2025-04-21 16:03:28 +0100

    Feat: Add Footer to design system

---
+++ src/design-system/components/Footer/Footer.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/react';
+import Footer from '.';
+
+export default {
+  name: 'Footer',
+  component: Footer,
+} as Meta;
+
+export const Default = () => <Footer />;
