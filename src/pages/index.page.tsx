ad655bad71fcaafb87e303c9a1e133e61c7a62ad
rosejbon <rosebonner.04@googlemail.com>
2025-07-05 18:18:36 +0100

    Feat: add new MapOverlay to various index pages

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import Head from 'next/head';
-import Box from '../components/Box';
-import Sidebar from '../design-system/components/Sidebar/Sidebar';
-import VisuallyHidden from '../design-system/utilities/VisuallyHidden';
-import { useMapState } from '../components/MapState';
 import { withApollo } from '../api-client/withApollo';
 import { useEffect } from 'react';
-import config from '../config';
 
-const SIDEBAR_BOTTOM_MARGIN = 32;
+import MapOverlay from '../design-system/components/MapOverlay';
+import Sidebar from '../design-system/components/Sidebar';
+import VisuallyHidden from '../design-system/utilities/VisuallyHidden';
+
+import { useMapState } from '../components/MapState';
+import config from '../config';
 
 const HomePage = () => {
   const [, setMapState] = useMapState();
@@ -26,20 +25,9 @@ const HomePage = () => {
 
       <VisuallyHidden as="div">{config.getTitle('Home')}</VisuallyHidden>
 
-      <Box
-        position="absolute"
-        zIndex={1}
-        top={0}
-        left={[0, 3]}
-        right={0}
-        m={3}
-        maxWidth={326}
-        maxHeight={`calc(100% - 0px - ${SIDEBAR_BOTTOM_MARGIN}px)`}
-        // center on small viewports
-        mx={['auto', 0]}
-      >
+      <MapOverlay>
         <Sidebar />
-      </Box>
+      </MapOverlay>
     </>
   );
 };
